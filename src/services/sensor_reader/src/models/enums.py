dd2f077eeaef65a1ab969737ab153a3d99b78507
JannikKmnn <JaKalkmann@web.de>
2025-11-13 12:01:12 +0100

    Added more positions and Sensor objects.

--- src/services/sensor_reader/src/models/enums.py
+++ src/services/sensor_reader/src/models/enums.py
@@ -1,12 +1,25 @@
 from enum import Enum
 
+
 class SensorType(str, Enum):
 
     TEMPERATURE = "Temperature"
     HUMIDITY = "Humidity"
     SOIL_MOISTURE = "SoilMoisture"
 
+
 class MeasureUnit(str, Enum):
 
     CELSIUS = "°C"
     PERCENT = "%"
+
+
+class Position(str, Enum):
+
+    LEFT = "Left"
+    RIGHT = "Right"
+    BACK = "Back"
+    FRONT = "Front"
+    UP = "Up"
+    MIDDLE = "Middle"
+    DOWN = "Down"
--- src/services/sensor_reader/src/models/sensor.py
+++ src/services/sensor_reader/src/models/sensor.py
@@ -1,9 +1,24 @@
 from pydantic import BaseModel
+from abc import ABC, abstractmethod
 
-from enums import MeasureUnit, SensorType
+from typing import Optional
+from enums import MeasureUnit, SensorType, Position
 
-class Sensor(BaseModel):
+
+class Sensor(ABC, BaseModel):
 
     identifier: str
     type: SensorType
     unit: MeasureUnit
+    position: Optional[Position] = None
+
+    @abstractmethod
+    async def measure(self):
+        pass
+
+
+class TemperatureSensor(Sensor):
+
+    async def measure(self, sensor_obj):
+        temperature_c = sensor_obj.get_temperature()
+        return temperature_c
