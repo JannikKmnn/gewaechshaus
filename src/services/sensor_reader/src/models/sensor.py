c3a613ffb83e9efa55d3e453b48dedb517fb1b9b
JannikKmnn <JaKalkmann@web.de>
2025-11-14 12:10:25 +0100

    Added Sensor defintions for models.

--- src/services/sensor_reader/src/models/sensor.py
+++ src/services/sensor_reader/src/models/sensor.py
@@ -1,16 +1,24 @@
 from pydantic import BaseModel
 from abc import ABC, abstractmethod
+from logging import Logger
 
-from typing import Optional
+from typing import Literal, Optional, Union
 from enums import MeasureUnit, SensorType, Position
+from decorators import retry
+
+from adafruit_dht import DHT11
+from w1thermsensor import W1ThermSensor
+import RPi.GPIO as GPIO  # type: ignore
 
 
 class Sensor(ABC, BaseModel):
 
     identifier: str
+    display_name: str
     type: SensorType
-    unit: MeasureUnit
+    unit: Optional[MeasureUnit] = None
     position: Optional[Position] = None
+    logger: Optional[Logger] = None
 
     @abstractmethod
     async def measure(self):
@@ -19,6 +27,38 @@ class Sensor(ABC, BaseModel):
 
 class TemperatureSensor(Sensor):
 
-    async def measure(self, sensor_obj):
-        temperature_c = sensor_obj.get_temperature()
+    sensor_obj: Union[W1ThermSensor, DHT11]
+
+    @retry(times=3)
+    async def measure(self):
+
+        if isinstance(self.sensor_obj, W1ThermSensor):
+            temperature_c = self.sensor_obj.get_temperature()
+        elif isinstance(self.sensor_obj, DHT11):
+            temperature_c = self.sensor_obj.temperature
+
         return temperature_c
+
+
+class SoilMoistureSensor(Sensor):
+
+    pin: int
+
+    @retry(times=2)
+    async def measure(self) -> Literal["wet", "dry"]:
+
+        value = GPIO.input(self.pin)
+        is_wet = value == GPIO.LOW
+
+        return "wet" if is_wet else "dry"
+
+
+class HumiditySensor(Sensor):
+
+    sensor_obj: DHT11
+
+    @retry(times=2)
+    async def measure(self) -> float:
+
+        humidity = self.sensor_obj.humidity
+        return humidity
