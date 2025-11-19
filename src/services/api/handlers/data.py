a9be0c1f811186e857c3e0a998731bcb56055bda
JannikKmnn <JaKalkmann@web.de>
2025-11-19 13:00:10 +0100

    Setup basic api structure.

---
+++ src/services/api/handlers/data.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from models.data import DataRequestProperties
+
+
+async def get_measurements(
+    req_properties: DataRequestProperties, sensor_identifier: Optional[str] = None
+):
+    pass
