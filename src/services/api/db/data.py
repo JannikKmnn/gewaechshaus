5567558cb180966802e057526602944c7d7f5538
JannikKmnn <JaKalkmann@web.de>
2025-11-25 00:08:43 +0100

    Small query addition.

--- src/services/api/db/data.py
+++ src/services/api/db/data.py
@@ -2,7 +2,7 @@ import os
 
 from datetime import datetime, timezone
 from fastapi import Depends
-from typing import Literal
+from typing import Literal, Optional
 
 from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 
@@ -13,9 +13,17 @@ async def get_measurements(
     measurement: Literal["AirPressure", "Humidity", "SoilMoisture", "Temperature"],
     end_time: datetime = datetime.now(tz=timezone.utc),
     influxdb_client: InfluxDBClientAsync = Depends(setup_client),
+    field_identifer: Optional[str] = None,
 ):
     
     query = f"""
         SELECT *
-        FROM
+        FROM {measurement}
+        WHERE time >= {str(start_time)} AND time <= {str(end_time)}
+        AND ({field_identifer} IS NOT NULL)
     """
+
+    async with influxdb_client:
+        table = await influxdb_client.query_api().query(
+            query=query
+        )
