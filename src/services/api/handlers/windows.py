030683f71370e797d1063cf6afee739d8536af3a
JannikKmnn <JaKalkmann@web.de>
2025-12-10 13:40:10 +0100

    Added endpoint to fetch window status

--- src/services/api/handlers/windows.py
+++ src/services/api/handlers/windows.py
@@ -22,3 +22,18 @@ async def close_window(actuator: LinearActuator):
             status_code=409,
             detail=f"{actuator.position.value} actuator is already closed.",
         )
+
+
+async def get_window_status(actuator: LinearActuator):
+
+    return_dict = {
+        "last_opening": actuator.last_extension,
+        "last_closing": actuator.last_retraction,
+    }
+
+    if actuator.is_extended:
+        return_dict["status"] = "open"
+    else:
+        return_dict["status"] = "closed"
+
+    return return_dict
--- src/services/api/router/windows.py
+++ src/services/api/router/windows.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request, HTTPException
 
-from src.services.api.handlers import windows
+try:
+    from src.services.api.handlers import windows
+except ModuleNotFoundError:
+    pass
 
 router = APIRouter()
 
@@ -45,3 +48,24 @@ async def close_window(window_position: str, request: Request):
         )
 
     _ = await windows.close_window(actuator=window_to_close)
+
+
+@router.get("/status/{window_position}", tags=["actuators"])
+async def get_window_status(window_position: str, request: Request):
+    window_actuators = request.app.state.actuators
+
+    requested_position = window_position.lower()
+
+    try:
+        window_to_close = next(
+            win
+            for win in window_actuators
+            if win.position.value.lower() == requested_position
+        )
+    except StopIteration:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Window on position {window_position} not initialized.",
+        )
+
+    return await windows.get_window_status(actuator=window_to_close)
