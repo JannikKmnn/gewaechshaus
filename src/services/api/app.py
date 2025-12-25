757cc68b6d3a044a6d45cb1c284a5be4effb4af9
JannikKmnn <JaKalkmann@web.de>
2025-12-25 18:07:17 +0100

    Added db setup to persistent storage

--- src/services/api/app.py
+++ src/services/api/app.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 from src.services.api.router.data import router as data_router
 from src.services.api.router.windows import router as windows_router
 
+from src.shared.sqlite import setup_db
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
 IMPORT_SUCCESS = False
 try:
     from src.shared.actuators import setup_window_openers
@@ -14,11 +19,28 @@ except ModuleNotFoundError:
     pass
 
 
+class Settings(BaseSettings):
+
+    # sqlite setup
+    sqlite_db_name = Field(default="/data/greenhouse.db")
+    sqlite_actuator_events_table = Field(default="window_status")
+
+
+settings = Settings()
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
     if IMPORT_SUCCESS:
         app.state.actuators = await setup_window_openers()
+
+    # Init sqlite DB for window status
+    # Expected to create only once because of persistent DB storage
+    _ = await setup_db(
+        db_name=settings.sqlite_db_name,
+        actuator_events_table=settings.sqlite_actuator_events_table,
+    )
 
     yield
 
--- src/shared/sqlite.py
+++ src/shared/sqlite.py
@@ -16,3 +16,6 @@ async def setup_db(
     """
 
     cur.execute(sql_stmt)
+
+    con.commit()
+    con.close()
