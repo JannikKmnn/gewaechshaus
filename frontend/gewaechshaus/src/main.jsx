2eab3c66d89dafdf233a5a7712c931d957940a86
JannikKmnn <JaKalkmann@web.de>
2026-01-12 21:27:42 +0100

    Basic setup and change to vite

---
+++ frontend/gewaechshaus/src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element = <h1>Hello Greenhouse!</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")).render(element);
