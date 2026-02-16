81c314ba0568b8b285561e9888b0be49379b7330
JannikKmnn <JaKalkmann@web.de>
2026-02-16 13:05:07 +0100

    Added last opening/closing to window control center

--- frontend/gewaechshaus/src/pages/ControlCenter.jsx
+++ frontend/gewaechshaus/src/pages/ControlCenter.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import BinaryWidget from "../components/BinaryWidget";
 import { getWindowStatus } from "../api/windows";
+import { formatDateTime } from "../utils/time";
 
 export default function ControlCenter() {
-  const [windowStatusLeft, setwindowStatusLeft] = useState(null);
-  const [windowStatusRight, setwindowStatusRight] = useState(null);
+  const [windowStatusLeft, setWindowStatusLeft] = useState(null);
+  const [windowStatusRight, setWindowStatusRight] = useState(null);
+
+  const [windowOpeningLeft, setWindowOpeningLeft] = useState(null);
+  const [windowOpeningRight, setWindowOpeningRight] = useState(null);
+
+  const [windowClosingLeft, setWindowClosingLeft] = useState(null);
+  const [windowClosingRight, setWindowClosingRight] = useState(null);
 
   useEffect(() => {
     async function getWindowsStatus() {
@@ -18,8 +25,12 @@ export default function ControlCenter() {
       ])
 
       if (results.length === 2) {
-        setwindowStatusLeft(results[0].status);
-        setwindowStatusRight(results[1].status);
+        setWindowStatusLeft(results[0].status);
+        setWindowStatusRight(results[1].status);
+        setWindowOpeningLeft(results[0].last_opening);
+        setWindowOpeningRight(results[1].last_opening);
+        setWindowClosingLeft(results[0].last_closing);
+        setWindowClosingRight(results[1].last_closing);
       }
       
     }
@@ -60,6 +71,26 @@ export default function ControlCenter() {
           fontsizelabel="12px"
         />
 
+      </div>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gap: "20px",
+          marginBottom: "10px"
+        }}
+      >
+
+        <div>
+          <div>Last Opening: {formatDateTime(windowOpeningLeft)}</div>
+          <div>Last Closing: {formatDateTime(windowClosingLeft)}</div>
+        </div>
+
+        <div>
+          <div>Last Opening: {formatDateTime(windowOpeningRight)}</div>
+          <div>Last Closing: {formatDateTime(windowClosingRight)}</div>
+        </div>
       </div>
     
     </div>
--- frontend/gewaechshaus/src/utils/time.js
+++ frontend/gewaechshaus/src/utils/time.js
@@ -7,3 +7,20 @@ export const formatTime = (isoString) => {
     minute: "2-digit",
   });
 };
+
+export function formatDateTime(isoString) {
+  if (!isoString) return "—";
+
+  const date = new Date(isoString);
+
+  return date
+    .toLocaleString("de-DE", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    })
+    .replace(",", "");
+}
