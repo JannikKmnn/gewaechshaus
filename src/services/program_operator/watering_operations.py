c9a5c184fa36e9c97c4d64b84922d8330e332175
JannikKmnn <JaKalkmann@web.de>
2026-08-03 13:02:02 +0200

    Implemented API call to run scheduled watering

--- src/services/program_operator/watering_operations.py
+++ src/services/program_operator/watering_operations.py
@@ -1,5 +1,29 @@
+import httpx
+
 from datetime import timedelta
+from logging import Logger
 
 
-async def run_scheduled_watering(duration: timedelta | None = None):
-    pass
+async def run_scheduled_watering(
+    url: str,
+    logger: Logger,
+    duration: timedelta | None = None,
+):
+    """
+    Calls watering API endpoint to start watering for a given duration.
+    If no duration is provided, it will use the default duration set in the API.
+    """
+
+    base_url = f"{url}/watering/run_watering"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                url=base_url,
+                json={"duration": duration.total_seconds() if duration else None},
+            )
+        except httpx.HTTPError as err:
+            logger.error(f"Watering call failed due to {err}")
+            response = None
+
+    return response
