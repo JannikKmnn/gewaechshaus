8172c2401d151ef0fafc99bffc20d6ebb32d773e
JannikKmnn <JaKalkmann@web.de>
2026-08-16 20:45:17 +0200

    Restructured picture locations

--- frontend/gewaechshaus/src/pages/Gallery.jsx
+++ frontend/gewaechshaus/src/pages/Gallery.jsx
@@ -1,3 +1,12 @@
 export default function Gallery() {
+
+  const [scrollModeActive, setscrollModeActive] = useState(false);
+
+  async function loadImages(path) {
+    var img = [];
+
+    
+  }
+
   return <div></div>;
 }
